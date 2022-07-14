0fe0e378b2c24c854a6686e8ca997f79050b14f1
GeonwooShin <rjsdnslawkd@naver.com>
2022-07-14 23:48:52 +0900

    애플리케이션 구조 변경

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../src/pages/Home";
 import Header from "./components/Header";
 import Login from "./pages/Login";
+import OAuth from "./pages/OAuth";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <div className="content_wrapper">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/login/oauth2/code/kakao" element={<OAuth />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
