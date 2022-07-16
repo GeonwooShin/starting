d144aa0944ecd79c37b696a8760887f338651d69
GeonwooShin <rjsdnslawkd@naver.com>
2022-07-16 11:43:12 +0900

    임의 배너 추가

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,11 +5,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-const bannerList = [
-  "https://www.namouriverlodge.com/wp-content/uploads/2018/09/web-design-banners-in-psd-photoshop-banner-website-banner-website-banners.jpg",
-  "https://marketplace.canva.com/EAEb577lR9o/2/0/1600w/canva-blue-modern-company-digital-web-agency-linkedin-banner-98UfUwHtDy8.jpg",
-  "https://www.vianet.com.np/wp-content/uploads/2021/03/web-banner-emi-easy-monthly-internet-2021-01-20.png",
-];
+const bannerList = ["배너1", "배너2", "배너3", "배너4"];
 
 const Banner = () => {
   return (
@@ -25,7 +21,7 @@ const Banner = () => {
     >
       {bannerList.map((banner) => (
         <SwiperSlide>
-          <img src={banner} alt="banner" />
+          <div className="banner-item">{banner}</div>
         </SwiperSlide>
       ))}
     </Swiper>
