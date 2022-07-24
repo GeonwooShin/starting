01e9791e75ee8c4389923820741da4833f295d55
GeonwooShin <rjsdnslawkd@naver.com>
2022-07-24 16:00:34 +0900

    임의 데이터통해서 grid 형태로 Ticket 컴포넌트 구현

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,21 +1,41 @@
 import React from "react";
 import TicketItem from "./TicketItem";
+import { Container, Grid } from "@mui/material";
+import { Link } from "react-router-dom";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const Ticket = () => {
   const TicketList = [
     { teamName: "순대남", num: 2, gender: "male" },
     { teamName: "하이요", num: 3, gender: "male" },
     { teamName: "여신모임", num: 2, gender: "female" },
-    { teamName: "재밌는애들", num: 4, gender: "male" },
+    { teamName: "재밌는애들모임", num: 4, gender: "male" },
     { teamName: "여기요", num: 4, gender: "male" },
     { teamName: "공주팟", num: 4, gender: "female" },
   ];
   return (
-    <div className="ticket_container">
-      {TicketList.map((item) => (
-        <TicketItem item={item} key={item.teamName} />
-      ))}
-    </div>
+    <Container maxWidth="lg" className="ticket_container">
+      <div className="service_title">
+        <div>
+          <Link to="/blinddate">
+            <h1 className="title">
+              과팅할래?
+              <ArrowForwardIosIcon />
+            </h1>
+          </Link>
+        </div>
+      </div>
+      <div className="service_subtitle">
+        <span className="subtitle">새로운 친구를 만나봐!</span>
+      </div>
+      <Grid style={{ margin: "0" }} container spacing={3} xs={12}>
+        {TicketList.map((item) => (
+          <Grid item md={4} xs={12}>
+            <TicketItem item={item} key={item.teamName} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
