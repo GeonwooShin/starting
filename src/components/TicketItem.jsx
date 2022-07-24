d5a61eb0f8aa7610a5164d79ee72c64f9a2d3bf8
GeonwooShin <rjsdnslawkd@naver.com>
2022-07-24 16:01:21 +0900

    Ticket svg item

--- src/components/TicketItem.jsx
+++ src/components/TicketItem.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const TicketItem = ({ item }) => {
   return (
-    <div>
+    <div className="ticket_img_container">
       <div className="ticket_img"></div>
+      <div className="ticket_team">
+        <span>{item.teamName}</span>
+      </div>
+      <div className="ticket_num">
+        <span>{item.num}</span>
+      </div>
+      <div className="ticket_gender">
+        <span>{item.gender}</span>
+      </div>
     </div>
   );
 };
