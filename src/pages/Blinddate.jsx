5797a4d14553d6b5a6facf218ea1d5f4317a1b66
GeonwooShin <rjsdnslawkd@naver.com>
2022-08-02 17:03:58 +0900

    과팅 페이지 기본 구조 추가

--- src/pages/Blinddate.jsx
+++ src/pages/Blinddate.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { Container, Button } from "@mui/material";
 
 const Blinddate = () => {
   return (
-    <>
-      <div className="Blinddate">Blinddate</div>
-    </>
+    <Container>
+      <div className="ticket-box-container">
+        <div className="ticket-box__push">
+          <div className="push-ticket-img"></div>
+          <Button size="small" variant="contained">
+            티켓 넣기
+          </Button>
+          <p className="ticket-box-description">
+            만들어진 팀들 중에 한 팀을 선택하여 과팅을 신청합니다.
+          </p>
+        </div>
+        <div className="ticket-box__pull">
+          <div className="pull-ticket-img"></div>
+          <Button size="small" variant="contained">
+            티켓 뽑기
+          </Button>
+          <p className="ticket-box-description">
+            과팅을 신청한 팀들 중 한 팀을 랜덤으로 뽑습니다.
+          </p>
+        </div>
+      </div>
+    </Container>
   );
 };
 
