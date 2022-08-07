74d161cc9becbbfd0bd701ac7ebdf687d1a80498
GeonwooShin <rjsdnslawkd@naver.com>
2022-08-07 15:01:49 +0900

    과팅 목록 타이틀 추가

--- src/pages/Blinddate.jsx
+++ src/pages/Blinddate.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Blinddate = () => {
   return (
@@ -23,6 +24,16 @@ const Blinddate = () => {
             과팅을 신청한 팀들 중 한 팀을 랜덤으로 뽑습니다.
           </p>
         </div>
+      </div>
+      <div className="ticket-service_title">
+        <div>
+          <h3 className="ticket_title">
+            아래의 팀들이 당신을 기다리고 있어요! ✨
+          </h3>
+        </div>
+      </div>
+      <div className="service_subtitle">
+        <span className="subtitle">티켓을 뽑아 만나보세요!</span>
       </div>
     </Container>
   );
