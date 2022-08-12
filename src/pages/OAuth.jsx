5b378dfac9e7b7e66548a82d77481105f1d70bd9
GeonwooShin <rjsdnslawkd@naver.com>
2022-08-12 22:16:20 +0900

    rest api kakao token 획득

--- src/pages/OAuth.jsx
+++ src/pages/OAuth.jsx
@@ -3,9 +3,28 @@ import React from "react";
 import { useEffect } from "react";
 
 const OAuth = () => {
-  const AthorizationURL = new URL(window.location.href).href;
+  const code = new URL(window.location.href).searchParams.get("code");
+  console.log(code);
   useEffect(() => {
-    axios.get(AthorizationURL).then((res) => console.log(res));
+    const data = {
+      grant_type: "authorization_code",
+      client_id: process.env.REACT_APP_REST_API_KEY,
+      redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+      code,
+      client_secret: process.env.REACT_APP_CLIENT_SECRET_KEY,
+    };
+    const queryString = Object.keys(data)
+      .map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(data[k]))
+      .join("&");
+
+    axios
+      .post("https://kauth.kakao.com/oauth/token", queryString, {
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        },
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   }, []);
   return <div>OAuth</div>;
 };
