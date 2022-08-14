333176543a3c9d424e8de52823bbf092259fc5d1
GeonwooShin <rjsdnslawkd@naver.com>
2022-08-14 23:13:14 +0900

    axios post에서 axios get으로 변경

--- src/pages/OAuth.jsx
+++ src/pages/OAuth.jsx
@@ -6,23 +6,8 @@ const OAuth = () => {
   const code = new URL(window.location.href).searchParams.get("code");
   console.log(code);
   useEffect(() => {
-    const data = {
-      grant_type: "authorization_code",
-      client_id: process.env.REACT_APP_REST_API_KEY,
-      redirect_uri: process.env.REACT_APP_REDIRECT_URI,
-      code,
-      client_secret: process.env.REACT_APP_CLIENT_SECRET_KEY,
-    };
-    const queryString = Object.keys(data)
-      .map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(data[k]))
-      .join("&");
-
     axios
-      .post("https://kauth.kakao.com/oauth/token", queryString, {
-        headers: {
-          "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-        },
-      })
+      .get("http://localhost:8080/oauth/redirect")
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   }, []);
@@ -30,3 +15,25 @@ const OAuth = () => {
 };
 
 export default OAuth;
+
+// useEffect(() => {
+//   const data = {
+//     grant_type: "authorization_code",
+//     client_id: process.env.REACT_APP_REST_API_KEY,
+//     redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+//     code,
+//     client_secret: process.env.REACT_APP_CLIENT_SECRET_KEY,
+//   };
+//   const queryString = Object.keys(data)
+//     .map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(data[k]))
+//     .join("&");
+
+//   axios
+//     .post("https://kauth.kakao.com/oauth/token", queryString, {
+//       headers: {
+//         "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+//       },
+//     })
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log(err));
+// }, []);
