fbcacd2629c673f574f0651a5b2116e65b495261
GeonwooShin <rjsdnslawkd@naver.com>
2022-09-07 01:04:29 +0900

    accessToken 받아오기

--- src/pages/OAuth.jsx
+++ src/pages/OAuth.jsx
@@ -1,39 +1,13 @@
-import axios from "axios";
 import React from "react";
 import { useEffect } from "react";
 
 const OAuth = () => {
-  const code = new URL(window.location.href).searchParams.get("code");
-  console.log(code);
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080/oauth/redirect")
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  }, []);
+  const accessToken = new URL(window.location.href).searchParams.get(
+    "accessToken"
+  );
+  console.log(accessToken);
+  useEffect(() => {}, []);
   return <div>OAuth</div>;
 };
 
 export default OAuth;
-
-// useEffect(() => {
-//   const data = {
-//     grant_type: "authorization_code",
-//     client_id: process.env.REACT_APP_REST_API_KEY,
-//     redirect_uri: process.env.REACT_APP_REDIRECT_URI,
-//     code,
-//     client_secret: process.env.REACT_APP_CLIENT_SECRET_KEY,
-//   };
-//   const queryString = Object.keys(data)
-//     .map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(data[k]))
-//     .join("&");
-
-//   axios
-//     .post("https://kauth.kakao.com/oauth/token", queryString, {
-//       headers: {
-//         "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-//       },
-//     })
-//     .then((res) => console.log(res))
-//     .catch((err) => console.log(err));
-// }, []);
