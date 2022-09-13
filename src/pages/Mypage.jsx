d0937f2dc14e3ee381186229b3d68c5dddce4440
semnil5202 <semnil5202@gmail.com>
2022-09-13 23:46:51 +0900

    마이페이지 구현

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Container } from "@mui/system";
+import Profile from "../components/Profile";
+import MyFriends from "../components/MyFriends";
+import MyTeams from "../components/MyTeams";
 
 const Mypage = () => {
   return (
-    <>
-      <div className="mypage">Mypage</div>
-    </>
+    <Container>
+      <Profile></Profile>
+      <div className="mypage-myfrineds-myteams">
+        <MyFriends></MyFriends>
+      </div>
+    </Container>
   );
 };
 
