fffdee7e14203ed4de4cd1ccd58fc628cb634a36
GeonwooShin <rjsdnslawkd@naver.com>
2022-09-26 00:26:53 +0900

    토큰 저장 구현

--- src/pages/OAuth.jsx
+++ src/pages/OAuth.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { useEffect } from "react";
+import { setLogin } from "../redux/slices/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const OAuth = () => {
   const accessToken = new URL(window.location.href).searchParams.get(
     "accessToken"
   );
   console.log(accessToken);
-  useEffect(() => {}, []);
-  return <div>OAuth</div>;
+  const token = useSelector((state) => state.userSlice.accessToken);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setLogin(accessToken));
+  }, []);
+  return <div>{token}</div>;
 };
 
 export default OAuth;
