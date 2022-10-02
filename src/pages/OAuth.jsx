0396d752883a30e75c03582054dcb523035e83af
GeonwooShin <rjsdnslawkd@naver.com>
2022-10-03 02:36:40 +0900

    로그인 구현

--- src/pages/OAuth.jsx
+++ src/pages/OAuth.jsx
@@ -1,19 +1,32 @@
-import React from "react";
 import { useEffect } from "react";
 import { setLogin } from "../redux/slices/userSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const OAuth = () => {
+  const navigate = useNavigate();
   const accessToken = new URL(window.location.href).searchParams.get(
     "accessToken"
   );
-  console.log(accessToken);
-  const token = useSelector((state) => state.userSlice.accessToken);
+  const memberId = new URL(window.location.href).searchParams.get("memberId");
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(setLogin(accessToken));
+    dispatch(setLogin({ accessToken, memberId }));
+    axios
+      .get(`http://13.209.146.204:8080/inputted/${memberId}`, {
+        headers: {
+          Authorization: "Bearer " + accessToken,
+        },
+      })
+      .then((res) => {
+        const { data } = res.data;
+        if (data) navigate("/");
+        else navigate("/signup");
+      })
+      .catch((err) => console.log(err.response));
   }, []);
-  return <div>{token}</div>;
+  return <div></div>;
 };
 
 export default OAuth;
