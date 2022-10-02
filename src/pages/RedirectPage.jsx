adcd079e4d1a5f0ea3b3cbf1f897b72da89cc045
GeonwooShin <rjsdnslawkd@naver.com>
2022-10-03 02:37:29 +0900

    홈화면 리다이렉트 페이지

--- src/pages/RedirectPage.jsx
+++ src/pages/RedirectPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Mypage from "./Mypage";
+import Login from "./Login";
 
 const RedirectPage = () => {
   const loginState = useSelector((state) => state.userSlice.loginState);
-  return loginState ? <Mypage /> : <Home />;
+  return loginState ? <Mypage /> : <Login />;
 };
 
 export default RedirectPage;
