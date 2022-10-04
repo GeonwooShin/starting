373eb367f1ce22f7fecbb8e7daac58213f7d56de
GeonwooShin <rjsdnslawkd@naver.com>
2022-10-04 20:12:14 +0900

    유저 정보 로드

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -3,11 +3,24 @@ import { Container } from "@mui/system";
 import Profile from "../components/Profile";
 import MyFriends from "../components/MyFriends";
 import MyTeams from "../components/MyTeams";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const Mypage = () => {
+  const [userInfo, setUserInfo] = useState({});
+  const memberId = useSelector((state) => state.userSlice.userId);
+  useEffect(() => {
+    axios
+      .get(`http://13.209.146.204:8080/mypage/${memberId}`)
+      .then((res) => {
+        setUserInfo(res.data.data);
+        console.log("회원 데이터 불러오기 완료");
+      })
+      .catch((err) => console.log(err.response));
+  }, []);
   return (
     <Container>
-      <Profile></Profile>
+      <Profile data={userInfo}></Profile>
       <div className="mypage-myfrineds-myteams">
         <MyFriends></MyFriends>
       </div>
